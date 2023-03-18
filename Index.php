cc6a01b0b3c79fca4ce8af6ddae056d70a411f4f
NorianHenrique <NorianHenrique@hotmail.com>
2023-03-18 18:23:51 -0300

    Segundo

---
+++ Index.php
@@ -0,0 +1,108 @@
+<html>
+    <head>
+        <title>Agencia Web</title>
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&amp;display=swap" rel="stylesheet">
+        <link href="https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+	    <link href="css/style.css" rel="stylesheet">
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+    </head>
+
+    <body>
+        <header>
+            <div class="logo">
+                 <h2>Agência Web</h2>
+            </div><!--logo-->
+
+            <nav class="menu-desktop">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav><!--menu-desktop-->
+
+            <div class="social">
+                <a target="_blank" href="https://facebook.com/dankicode"><i class="icon-facebook-sign"></i></a>
+				<a target="_blank" href="https://instagram.com/guigrillo13"><i class="icon-instagram"></i></a>
+            </div><!--social-->
+            <div class="clear"></div>
+        </header>
+
+        <section class="apresentacao">
+            <div class="center">
+                <div class="sobre-top">
+                   <h3>De ponta a ponta</h3>
+                   <h2>Somos  <span> criativos</span>  e focados em <span> resultados</span> </h2>
+                  </br>
+                  <p>Uma equipe jovem e altamente qualificada garante o sucesso do seu projeto e produto. Especialistas em suas áreas, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  </br>
+                   <a href="">Entre em Contato!</a>
+                </div><!--sobre-top-->
+             <div class="img-sobre">
+                   <img src="Imagens/image-hero.png"/>
+             </div><!--img-sobre-->
+             <div class="clear"></div>
+            </div>
+        </section><!--apresentacao-->
+
+        <section class="trabalhos">
+           <div class="center">
+               <h4>Nossos serviços</h4>
+               <h2>O que Fazemos</h2>
+            <div class="trabalhos-single">
+              <img src="Imagens/image-oquefazemos.png"/>
+              <h3>"Temos a Agência Loop como parceiro estratégico nos nossos planejamento em plataforma digital. Super recomendo!"</h3>
+              <p>Luiza Oliveira - Indústrias Gramps</p>
+            </div><!--trabalhos-single-->
+
+            <div class="trabalhos-wraper">
+              <img src="Imagens/sistemasesites.png"/>
+              <h5>Sites e Sistemas</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+             </br>
+             </br>
+             <img src="Imagens/comunicacaoeseo.png"/>
+             <h5>Comunicação e SEO</h5>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div><!--trabalhos-wraper-->
+           </br>
+           </br>
+          
+            <img src="Imagens/depoimento.png"/>
+            <div class="clear"></div>
+           </div>
+        </section><!--trabalhos-->
+
+        <section class="projetos">
+           <div class="center">
+               <h4>Nossos Trabalhos</h4>
+               <h2>O que fizemos</h2>
+           <div class="img-projetos">
+              <img src="Imagens/portfolio-3.png"/>
+           </div><!--img-projetos-->
+           </div>
+        </section><!--projetos-->
+
+        <section class="contato">
+            <div class="center">
+               <h4>Entre em contato</h4>
+               <h2>Como podemos te ajudar?</h2>
+     
+            <div class="formulario">
+                <form method="POST">
+                     <input type="text" name="nome" placeholder="Seu nome.."/>
+                     <input type="email" name="email" placeholder="Seu email.."/>
+                     <textarea name="mensagem" placeholder="Sua mensagem.."></textarea>
+                     <input type="submit" name="acao" value="Enviar!"/>
+                </form> 
+            </div><!--formulario-->
+            </div>
+        </section><!--contato-->
+
+        <footer>
+            <h2>Feito com<i class="icon-heart"></i>por Agência Loop!</h2>
+        </footer>
+
+    </body>
+</html>
